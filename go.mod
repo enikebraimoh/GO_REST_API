58668d2414d3cd5650ceab4c4728fe05402437b5
enikebraimoh <richardbraimoh@gmail.com>
2021-08-25 16:34:50 +0100

    added endpoints

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/enikebraimoh/GO_REST_API
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	//init router
+	router := mux.NewRouter()
+
+}
